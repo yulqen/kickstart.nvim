0b376bc74822c0ba01c8549575eb7693763d9fc4
Matthew Lemon <y@yulqen.org>
2024-04-30 08:44:55 +0100

    Minor clean-up

--- init.lua
+++ init.lua
@@ -309,11 +309,6 @@ require('lazy').setup({
     'sourcegraph/sg.nvim',
     dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
     opts = {},
-
-    -- If you have a recent version of lazy.nvim, you don't need to add this!
-    --     build = "nvim -l build/init.lua",
-    --       },
-    --       }]]}
   },
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
