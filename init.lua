3d6513a60ab656abd8583189641b7811a5465b0c
Matthew Lemon <y@yulqen.org>
2024-05-16 15:20:18 +0100

    Add tab keyboard shortcut for move tabs

--- init.lua
+++ init.lua
@@ -199,6 +199,8 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', '<tab>', ':tabnext<CR>', { desc = 'Move to next tab' })
+
 -- neotest keybinds
 vim.keymap.set('n', 'dn', ':lua require("neotest").run.run({strategy = "dap"})')
 vim.keymap.set('n', 'tn', ':lua require("neotest").run.run()<CR>', { desc = 'Test nearest' })
