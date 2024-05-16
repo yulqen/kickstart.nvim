da9e9b0eeee066671431027479bc411e0417cae6
Matthew Lemon <y@yulqen.org>
2024-05-16 15:41:04 +0100

    Implement formatting on save in python?

--- init.lua
+++ init.lua
@@ -940,7 +940,14 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { { 'ruff', 'isort', 'black' } },
+        python = function(bufnr)
+          if require('conform').get_formatter_info('ruff_format', bufnr).available then
+            return { 'ruff_format' }
+          else
+            return { 'isort', 'black' }
+          end
+        end,
+        ['_'] = { 'trim_whitespace' },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
