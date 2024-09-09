4f24999f532d1ce4d20da66bfd587c66d7f6918b
Matthew Lemon <y@yulqen.org>
2024-09-09 15:50:25 +0100

    Adds ayu colourscheme

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 --[[
-
+init
 =====================================================================
 ==================== READ THIS BEFORE CONTINUING ====================
 =====================================================================
@@ -87,7 +87,7 @@ P.S. You can delete this when you're done too. It's your config now! :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ','
+vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
@@ -1077,18 +1077,21 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     -- 'folke/tokyonight.nvim',
-    'ellisonleao/gruvbox.nvim',
+    --'ellisonleao/gruvbox.nvim',
+    'ayu-theme/ayu-vim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'darkblue'
+      -- vim.cmd.colorscheme 'darkblue'
 
-      -- vim.cmd 'set termguicolors'
+      vim.cmd 'set termguicolors'
+      vim.cmd.colorscheme 'ayu'
+      vim.g.ayucolor = 'dark'
 
       -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      -- vim.cmd.hi 'Comment gui=none'
     end,
   },
 
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+
+  {
+    'ayu-theme/ayu-vim',
+    opts = {
+      ayucolor = 'dark',
+    },
+  },
+}
